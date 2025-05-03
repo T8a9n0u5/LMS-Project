4087989676c7d85c36a06fac6c24b3b68cd0411d
Tanu <sainitanu07643@gmail.com>
2025-05-03 15:32:55 +0530

    finally

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -10,7 +10,7 @@ import User from "../models/User.js";
 
 export const clerkWebhooks = async (req, res) => {
     try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK-SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
